3f4035f1c84ccf2555344cf461e83bd0704a760c
Luca Caprini <luca.caprini@eng.it>
2018-02-07 17:59:55 +0100

    Concluso modulo device

--- src/providers/device/device.service.ts
+++ src/providers/device/device.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Dialogs } from '@ionic-native/dialogs';
 import { Keyboard } from '@ionic-native/keyboard';
 import { Network } from '@ionic-native/network';
 import { SpinnerDialog } from '@ionic-native/spinner-dialog';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { Platform } from 'ionic-angular';
 import { LoadingController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 import { Loading } from 'ionic-angular/components/loading/loading';
 import { Observable } from 'rxjs/Observable';
 
+import { ConfirmButton } from './ConfirmButton.model';
 import { DeviceConfig } from './device.config';
 
 @Injectable()
@@ -23,7 +25,8 @@ export class DeviceService {
         private splashScreen: SplashScreen,
         private keyboard: Keyboard,
         private spinnerDialog: SpinnerDialog,
-        private loadingCtrl: LoadingController
+        private loadingCtrl: LoadingController,
+        private dialogs: Dialogs
     ) {
         // Create two Observable for online and offline notifications
         // to allow the whole app to subscribe to them with the custom functions
@@ -76,8 +79,7 @@ export class DeviceService {
     isOnline(): boolean {
         if (this.isCordova()) {
             const connectionType = this.network.type
-            // tslint:disable-next-line
-            return connectionType !== window.Connection.UNKNOWN && connectionType !== window.Connection.NONE;
+            return connectionType !== (window as any).Connection.UNKNOWN && connectionType !== (window as any).Connection.NONE;
         }
         else {
             return (window.navigator && window.navigator.onLine);
@@ -156,6 +158,8 @@ export class DeviceService {
     showLoading(message: string = ''): void {
         this.closeKeyboard();
 
+        // message = Translator.t(message);
+
         if (this.isCordova()) {
             this.spinnerDialog.show(this.config.getModalTitle(), message, true);
         }
@@ -182,173 +186,68 @@ export class DeviceService {
         }
     }
 
+
+    /**
+    * Show a native alert dialog or a simple browser alert
+    * @param  {string} message Dialog message
+    * @param  {string} title Dialog title
+    * @returns void
+    */
+    alert(message: string, title: string = this.config.getModalTitle()): void {
+        this.hideLoading();
+
+        let okButton = 'OK';
+        // try {
+        //     text = Translator.t(text);
+        //     title = Translator.t(title);
+        //     okButton = Translator.t(okButton);
+        // }
+        // catch (e) {}
+
+        if (this.isCordova()) {
+            this.dialogs.alert(message, title, okButton);
+        }
+        else {
+            window.alert(message);
+        }
+    }
+
+
+    /**
+    * Show a native confirm dialog or the simple browser confirm
+    * @param {string} message Dialog message
+    * @param {string} title Dialog title
+    * @param {ConfirmButton[]} buttons List of <ConfirmButton>
+    */
+    confirm(message: string, title: string = this.config.getModalTitle(), buttons: ConfirmButton[] = [new ConfirmButton('Ok'), new ConfirmButton('Cancel')]) {
+        this.hideLoading();
+
+        // message = Translator.t(message);
+        // title = Translator.t(title);
+        const buttonLabels = buttons.map((b: ConfirmButton) => {
+            // return Translator.t(b.title);
+            return b.title;
+        });
+
+        if (this.isCordova()) {
+            this.dialogs.confirm(message, title, buttonLabels).then(
+                (buttonIndex: number) => {
+                    // Decrement clicked button index because the plugin use 'one-based indexing'
+                    buttonIndex--;
+                    // Then execute the 'onClick' function if is defined
+                    if (buttons[buttonIndex]) {
+                        buttons[buttonIndex].onClick();
+                    }
+                }
+            );
+        }
+        else {
+            if (window.confirm(message)) {
+                buttons[0].onClick();
+            }
+            else {
+                buttons[1].onClick();
+            }
+        }
+    }
 }
-
-
-// import device from 'device.js';
-// import _ from 'lodash';
-// import f7Config from '../../f7.config.js';
-// import Translator from 'js/utils/Translator';
-// const STATUSBAR_THEMES_COLORS = {
-//     red        : '#f44336',
-//     pink       : '#e91e63',
-//     purple     : '#9c27b0',
-//     deeppurple : '#673ab7',
-//     indigo     : '#E10098',
-//     blue       : '#2196f3',
-//     lightblue  : '#03a9f4',
-//     cyan       : '#00bcd4',
-//     teal       : '#009688',
-//     green      : '#4caf50',
-//     lightgreen : '#8bc34a',
-//     lime       : '#cddc39',
-//     yellow     : '#ffeb3b',
-//     amber      : '#ffc107',
-//     orange     : '#ff9800',
-//     deeporange : '#ff5722',
-//     brown      : '#795548',
-//     gray       : '#9e9e9e',
-//     bluegray   : '#607d8b',
-//     white      : '#ffffff',
-//     black      : '#000000'
-// };
-
-// class Device {
-//     constructor() {
-//         // Attuale theme impostato sulal statusbar
-//         this._statusBarTheme = null;
-//     }
-
-
-//     /**
-//      * Mostra una notifica sul device
-//      * @param {Object} options Tutte le opzioni fornite dal notification di F7
-//      */
-//     showNotification(options = {}) {
-//         if (!options.message) {
-//             options.message = 'Notification';
-//         }
-//         options.message = (options.message);
-//         if (!options.button) {
-//             options.button = {
-//                 text  : ('CLOSE'),
-//                 color : 'teal',
-//                 close : true
-//             };
-//         }
-//         if (!options.hold) {
-//             options.hold = 2000;
-//         }
-//         window.f7.addNotification(options);
-//     }
-
-//     /**
-//      * Mostra un avviso all'utente
-//      * @param {String} text - Corpo dell'avviso
-//      * @param {String} title - Se esiste è il titolo dell'avviso
-//      * @param {function} callback - Callback da eseguire alla pressione del tasto OK
-//      */
-//     alert(text, title, callback) {
-//         this.hideLoader();
-
-//         if (!title) {
-//             title = f7Config.modalTitle;
-//         }
-
-//         let okButton = 'OK';
-//         try {
-//             text = Translator.t(text);
-//             title = Translator.t(title);
-//             okButton = Translator.t(okButton);
-//         }
-//         catch (e) {}
-
-//         if (this.isCordova() && navigator.notification) {
-//             navigator.notification.alert(
-//                 text,
-//                 callback,
-//                 title,
-//                 okButton
-//             );
-//         }
-//         else if (window.f7) {
-//             window.f7.alert(text, title, callback);
-//         }
-//         else {
-//             window.alert(text);
-//         }
-//     }
-
-//     /**
-//      * Mostra una richiesta di conferma all'utente
-//      * @param {String} text - Corpo del confirm
-//      * @param {String} title - Se esiste è il titolo dell'avviso
-//      * @param {String[]} buttons - Lista dei tasti da mostrare con titolo e callback
-//      */
-//     confirm({text, title, buttons = [{title : 'Ok'}, {title : 'Cancel'}]}) {
-//         this.hideLoader();
-
-//         if (!title) {
-//             title = f7Config.modalTitle;
-//         }
-
-//         // Traduco i testi dei bottoni
-//         const translatedButtons = _.map(buttons, b => {
-//             return Translator.t(b.title);
-//         });
-
-//         if (this.isCordova() && navigator.notification) {
-//             navigator.notification.confirm(
-//                 Translator.t(text),
-//                 buttonIndex => {
-//                     // Se definita lancio la callback del bottone premuto
-//                     buttonIndex--;
-//                     if (buttons[buttonIndex] && buttons[buttonIndex].onClick) {
-//                         buttons[buttonIndex].onClick();
-//                     }
-//                 },
-//                 Translator.t(title),
-//                 translatedButtons
-//             );
-//         }
-//         else {
-//             if (window.confirm(Translator.t(text))) {
-//                 if (buttons[0] && buttons[0].onClick) {
-//                     buttons[0].onClick();
-//                 }
-//             }
-//             else {
-//                 if (buttons[1] && buttons[1].onClick) {
-//                     buttons[1].onClick();
-//                 }
-//             }
-//         }
-//     }
-
-//     /**
-//      * Mostra una modale all'utente
-//      * @param {String} title - Titolo della modale
-//      * @param {String} text - Messaggio della modale
-//      * @param {String} afterText - Messaggio o HTML mostrato sotto al `text`
-//      * @param {String[]} buttons - Lista dei tasti da mostrare
-//      * @param {Boolean} verticalButtons - Flag per indicare la visualizzazione verticale dei tasti
-//      * @param {Function} onClick - Callback da eseguire a seguito del click su uno qualsiasi dei tasti
-//      */
-//     modal({title, text = '', afterText = null, buttons = [{text : 'OK'}], verticalButtons = false, onClick}) {
-//         this.hideLoader();
-
-//         if (!title) {
-//             title = f7Config.modalTitle;
-//         }
-
-//         window.f7.modal({
-//             title : Translator.t(title),
-//             text  : Translator.t(text),
-//             afterText,
-//             buttons,
-//             verticalButtons,
-//             onClick});
-//     }
-// }
-
-// export default new Device();
