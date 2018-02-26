7ee571b66ccb35f395a724974500ccbc44babfa2
Luca Caprini <luca.caprini@eng.it>
2018-02-26 15:31:57 +0100

    Aggiunto modulo DB con LokiJS

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ENV } from '@env';
+import { DBModule } from '@shared/db/db.module';
 import { I18nModule } from '@shared/i18n/i18n.module';
 
 @NgModule({
@@ -7,7 +8,8 @@ import { I18nModule } from '@shared/i18n/i18n.module';
         I18nModule.forRoot({
             url: ENV.translationsUrl,
             storePrefix: ENV.appName.replace(/ /g, '')
-        })
+        }),
+        DBModule
     ],
     exports : [
         I18nModule
