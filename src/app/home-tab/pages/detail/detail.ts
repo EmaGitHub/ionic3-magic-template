6d6a6e49f0d454f4a06d5c48f2779a8c364664d8
Emanuele <Calvisi>
2019-11-21 16:08:40 +0100

    insert listener in ApiCall

--- src/app/home-tab/pages/detail/detail.ts
+++ src/app/home-tab/pages/detail/detail.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FadeInOut } from '@app/core/animations';
-import { ApiService, Backend } from '@app/core/api';
 import { AppStore } from '@app/app-store';
 import { Store } from '@ngrx/store';
 import { ApiCallAction } from '@app/core/api/actions/api-call-action';
-import { Subscription } from 'rxjs';
 import { ApiCallState } from '@app/core/api/redux-models/api-state';
 
 @Component({
@@ -14,14 +12,14 @@ import { ApiCallState } from '@app/core/api/redux-models/api-state';
         FadeInOut,
     ]
 })
-export class DetailPage implements OnInit {
+export class DetailPage implements OnInit, OnDestroy {
 
     private text: string = " similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
     private visibleText: string = '0';
     public getResp: any[] = [];
     public postResp: any = null;
 
-    private apiStateSubscription$?: Subscription;
+    private apiStateSubscription$= this.store.select('apiState').take(2);
 
     constructor(
         private store: Store<AppStore>
@@ -29,10 +27,14 @@ export class DetailPage implements OnInit {
 
     ngOnInit() {
 
-        this.apiStateSubscription$ = this.store.select('apiState').subscribe((state: ApiCallState) => {
-           
-            if (state.requestPending == false && state.error==null){
+       
+    }
 
+    listenForResponse(){
+
+        this.apiStateSubscription$.subscribe((state: ApiCallState) => {
+
+            if (state.requestPending == false && state.error==null){
                 if (state.apiName == 'get1') this.getResp = state.response
                 if (state.apiName == 'post') this.postResp = JSON.stringify(state.response)
             }
@@ -49,12 +51,20 @@ export class DetailPage implements OnInit {
     makeGetReq() {
 
         this.store.dispatch(new ApiCallAction('get1', {}));
+        this.listenForResponse();
     }
 
     makePostReq() {
-
+        
         this.store.dispatch(new ApiCallAction('post', {body: {id: 1, title: 'title', userId: '63'}}));
+        this.listenForResponse();
+    }
 
+    ngOnDestroy(){
+
+        console.log("ngOnDestroy");
+        this.getResp = [];
+        this.postResp = null;
     }
 
 }
